4a5470894477415486ea34fa5adbb26bef4eb9b6
Zack Brandes <brandesz@amazon.com>
2022-09-25 15:35:50 +0200

    Fixed builder

--- lib/view/diagram/staff_widget.dart
+++ lib/view/diagram/staff_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 
-// part staff_widget.g.dart;
+part 'staff_widget.g.dart';
 
 @swidget
 Widget staffWidget(BuildContext context) {
